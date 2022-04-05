b918c96305e18870430249527b69c885e590a287
canyang2022 <can.yang.2@volvocars.com>
2022-04-05 23:46:25 +0200

    miniproject add book and buy book

--- mini project/bookShop.cpp
+++ mini project/bookShop.cpp
@@ -1,30 +1,74 @@
 //book shop
+/*A book shop maintains the inventory of books that are being sold at the shop. The list includes details such as
+author, title, price, publisher and number of copies. Whenever a customer wants a book, the sales person
+inputs the title and author and the system searches the list and displays whether it is available or not. If it is not,
+an appropriate message is displayed. During the buying process the system displays the book details and
+requests for the number of copies required. If the requested copies are available, the total cost of the requested
+copies is displayed; otherwise the message “Requiredcopies not in stock”is displayed.
+Design a system using a class called books with suitable member functions and Constructors. Use new operator
+in constructors to allocate memory space required. Implement C++ program for the system.*/
+
 #include "books.hpp"
 
 
 void meny_display();
 
 int main(){
+
+    Book* library[50];
+    unsigned int bookmark =0;
+
+    /*for(int i=0;i<10;i++){
+        library[i]=NULL;
+    }*/
+
     while(true){
 
         meny_display();
         unsigned int num;
         unsigned int numbersOfBook;
 
+
         std::cout << "Enter your choice: ";
         std::cin>>num;
 
         switch(num){
             case 1:
-                std::cout<<"how many books do you want to add?\n";
-                std::cin >> numbersOfBook;
-                Books* library;
-
-                for(int i=0; i < numbersOfBook; i++){
-                    Books* bok= new Books();
-                    library[i]= *bok;
+            {
+                Book* bok= new Book();
+                library[bookmark]= bok;
+                bookmark++;
+                break;
+            }
+            case 2:
+            {
+                std::string book_name;
+                std::string author_fir_name;
+                std::string author_sec_name;
+                std::cout<<"What is the name of the book you want to buy? \n";
+                std:: cin >> book_name;
+                std::cout<<"Enter author first name \n";
+                std:: cin >> author_fir_name;
+                std::cout<<"Enter author second name \n";
+                std:: cin >> author_sec_name;
+                for (int i=0; i< bookmark; i++){
+                    if((library[i]->tittle == book_name)&&(library[i]->authorfirst == author_fir_name)
+                    &&(library[i]->authorsecond == author_sec_name)){
+                        std::cout<< "We have the book you want";
+                    }else
+                    {std::cout<<"Sorry, we dont have the book you want to buy now.\n";
+                    }
                 }
+
+                break;
+            }
+
+            case 3:
+                break;
+
+
         };
+        std::cout <<"authorname"<< library[bookmark-1];
     };
 }
 
--- mini project/books.hpp
+++ mini project/books.hpp
@@ -2,38 +2,28 @@
 #include<iostream>
 
 // Book, author, title, price, publisher and number of copies
-class Books
+class Book
 {
 
 public:
     std::string authorfirst;
     std::string authorsecond;
-    /*std::string tittle;
+    std::string tittle;
     std::string publish_name;
     std::string price;
-    std::string copies;*/
+    std::string copies;
 
-    Books();
-    ~Books();
-    void print(){
-        std::cout << authorfirst << std::endl;
-        std::cout << authorsecond << std::endl;
-        /*std::cout << publish_name << std::endl;
-        std::cout << price << std::endl;
-        std::cout << copies << std::endl;
-        std::cout << std::endl;*/
-    }
-
+    Book();
 };
-Books::Books()
+Book::Book()
 {
 
     std::cout << "Enter Author First Name: ";
     std::cin >> authorfirst;
     std::cout << "Enter Author Second Name: ";
     std::cin >> authorsecond;
-//  std::cout << "Enter Tittle Name: ";
-//  std::cin >> tittle;
+    std::cout << "Enter Tittle Name: ";
+    std::cin >> tittle;
 //  std::cout << "Enter Publisher Name: ";
 //  std::cin >> publish_name;
 //  std::cout << "Enter Price: ";
