2f963a63291c31f6bd2c5184b0c78165f7e403ac
canyang2022 <can.yang.2@volvocars.com>
2022-04-09 19:12:44 +0200

    1024 assignment draft

--- W3Day3/Assignment3.cpp
+++ W3Day3/Assignment3.cpp
@@ -2,11 +2,18 @@
 closest to the given number (smaller, and bigger) with less than twelve lookups.*/
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 void generate_numbers(std::vector<int>*);
+void search_number(std::vector<int>*, int);
 
 int main(){
     std::vector<int> v;
+    // the number needs to find the place
+    int number;
+    std::cout<<"type a random number: \n";
+    std::cin>> number;
+
     // store the ranom number in the vector
     generate_numbers(&v);
 
@@ -14,8 +21,7 @@ int main(){
     for(int i=0;i<v.size();i++){
         std::cout<< v[i]<<" ";
     }
-
-
+    search_number(&v, number);
 
 }
 
@@ -24,8 +30,23 @@ void generate_numbers(std::vector<int>* v)
     for(int i=0;i<1024;i++){
 
         // random number generator
-        int num = rand()%10000;
+        int num = rand()%1000;
         (*v).push_back(num);
+        std::sort((*v).begin(),(*v).end());
 
      }
+}
+
+void search_number(std::vector<int>* v, int number){
+
+   std::vector<int>::iterator upper, lower;
+   (*v).erase(upper, lower);
+   upper=std::upper_bound((*v).begin(),(*v).end(),number);
+   lower=std::lower_bound((*v).begin(),(*v).end(),number);
+    //(*v).erase(upper, lower);
+
+
+   std::cout<<"The upper bound is: "<<upper[0]<<'\n';
+   std::cout<<"The lower bound is: "<<lower[0]<<'\n';
+
 }
